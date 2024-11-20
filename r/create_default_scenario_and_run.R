565dc6b5186b21219fb75b961571bda980b2d9b8
David J. Smith <smith.david@epa.gov>
2024-11-20 12:22:27 -0500

    Add create_default_scenario_and_run.

--- r/create_default_scenario_and_run.R
+++ r/create_default_scenario_and_run.R
@@ -1,10 +1,9 @@
-# Create a project with no scenarios.
-# HRU settings match what is used for ICLUS
-# Poll results of project creation every 10 seconds until progress is 100%
-# Save project files to a directory - no scenario has been added or run yet, so these are watershed files such as subbasins, HRUs, and point source samples.
+# Create a default scenario attached to the supplied project request ID and run it.
+# Provide project request ID as a command line argument.
 #
-# r create_project.R
+# python create_default_scenario_and_run.R <project_request_id>
 #
+
 
 library(httr2)
 library(jsonlite)
@@ -12,7 +11,6 @@ library(jsonlite)
 source("api_helpers.R")
 
 #Get app settings
-
 Appsettings = getAppsettings('appsettings.json')
 
 connection = Appsettings[['connection']]
@@ -21,21 +19,29 @@ savePath = Appsettings[['savePath']]
 apiUrl = Appsettings[['apiUrl']]
 
 #Define how frequently to check the project's creation status (seconds)
-pollInterval = 10
+pollInterval = 10 
 
 input_data <- list(
-  dataset = 'HUC8',
-  downstreamSubbasin = '07100009',
-  setHrus = list(
-    method = 'area',
-    target = 1,
-    units = 'km2'
+    projectRequestId = as.integer(commandArgs(trailingOnly = TRUE)[1]),
+    weatherDataset = "PRISM",
+    startingSimulationDate = "1981-01-01",
+    endingSimulationDate = "1985-12-31",
+    warmupYears = 2,
+    outputPrintSetting = "daily",
+    reportData = list(
+      formats = list("csv", "netcdf"),
+      units = "metric",
+      outputs = list(
+        rch = list(
+          statistics = list("daily_avg")
+      )
+    )
   )
 )
 
 #Submit the request to the API and read the response
 connection.request = request(apiUrl) |> 
-  req_url_path("/builder/project/create-only") |>
+  req_url_path("/builder/scenario/create-and-run") |>
   req_headers(!!!headers) |>
   req_body_json(input_data)
 
@@ -47,28 +53,31 @@ print(paste("Project request ID", submissionResult[['id']], "submitted"))
 spaces = 0
 
 repeat {
-  project <- getStatus(headers, submissionResult[['url']], spaces)[['project']]
-  if (project[['status']][['progress']] >= 100) {
+  scenario <- getStatus(headers, submissionResult[['url']], spaces)[['project']]
+  if (scenario[['status']][['progress']] >= 100) {
     cat("\n")
     break
   }
   Sys.sleep(pollInterval)
 }
 
-error = project['status']['errorStackTrace'][[1]]
+error = scenario['status']['errorStackTrace'][[1]]
 if (!is.null(error)){
   #print('Error stack trace: {}'.format(error))
   print(paste("Error stack trace:", error))
 }
 
-#Save project files to disk; will include HRUs CSV, subbasins CSV and watershed files (including point source samples)
-requestPath = file.path(savePath, paste0("Project_",submissionResult[['id']]))
+#Save scenario output files to disk
+requestPath = file.path(savePath, paste0("Scenario_",submissionResult[['id']]))
 dir.create(requestPath, recursive=TRUE)
 
-for (file in project[['output']]) {
+
+for (file in scenario[['output']]) {
   cat("Retrieving and saving ", file[['name']], " (", file[['format']], ")\n", sep = "")
   filename <- tail(strsplit(file[['url']], "/")[[1]], 1)
   download.file(file[['url']], destfile = file.path(requestPath, filename), mode = "wb")
 }
 
-print(paste("Project request ID", submissionResult[['id']], "creation complete"))
+print(paste("Scenario request ID", submissionResult[['id']], "creation complete"))
+
+
